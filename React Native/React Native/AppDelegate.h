950df5a4a2928b08909a1b2902b3256d77672ca3
weixiaoyang <weixiaoyang@weixiaoyangdeimac.local>
2016-11-03 15:49:02 +0800

    初始化项目

---
+++ React Native/React Native/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  React Native
+//
+//  Created by weixiaoyang on 2016/11/3.
+//  Copyright © 2016年 weixiaoyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ React Native/React Native/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  React Native
+//
+//  Created by weixiaoyang on 2016/11/3.
+//  Copyright © 2016年 weixiaoyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
